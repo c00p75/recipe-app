bbf670f0a4937169447a758b1006e34431835044
yodit93 <yoditabebe9391@gmail.com>
2023-06-26 22:08:32 +0300

    Generate migration file to Update the price data type

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_184047) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_190119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
-    t.integer "price"
+    t.string "price"
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
