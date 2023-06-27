7deb1a71fb5f5dfeef1a7166eb1385b67512340e
yodit93 <yoditabebe9391@gmail.com>
2023-06-27 09:10:40 +0300

    Implement food destroy method

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,17 @@
 class FoodsController < ApplicationController
+    before_action :set_user, only: %i[index destroy]
     def index
-        @user = User.find(params[:user_id])
         @foods = @user.foods
+    end
+
+    def destroy
+        @food = @user.foods.find(params[:id])
+        @food.destroy
+        redirect_to user_foods_path(@user), notice: 'Food deleted successffuly'
+    end
+
+    private
+    def set_user
+        @user = User.find(params[:user_id])
     end
 end
