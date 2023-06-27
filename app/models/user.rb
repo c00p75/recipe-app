185bec2fdf112f682925cd1960123a2ed5e71c47
c00p75 <georgecoopmsapenda@gmail.com>
2023-06-27 10:56:16 +0200

    Define association between users and recipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  # Define associations
   has_many :foods
+  has_many :recipes
 end
