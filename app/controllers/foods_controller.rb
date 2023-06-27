df685d4d9f1abb0d5d30a6650b38a76c3fafc9c0
yodit93 <yoditabebe9391@gmail.com>
2023-06-27 09:55:29 +0300

    Implement food create method

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,20 @@
 class FoodsController < ApplicationController
-    before_action :set_user, only: %i[index destroy]
+    before_action :set_user, only: %i[index create destroy]
     def index
         @foods = @user.foods
+    end
+
+    def new
+        @food = Food.new
+    end
+
+    def create
+        @food = @user.foods.new(food_params)
+        if @food.save
+            redirect_to user_foods_path(@user)
+        else
+            render :new
+        end
     end
 
     def destroy
@@ -11,6 +24,11 @@ class FoodsController < ApplicationController
     end
 
     private
+
+    def food_params
+        params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+    end
+
     def set_user
         @user = User.find(params[:user_id])
     end
