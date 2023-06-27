4a632c51e7499a05989ef65323e9cc21e5f0bacd
yodit93 <yoditabebe9391@gmail.com>
2023-06-27 10:38:13 +0300

    Add styles for food form

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,35 +1,35 @@
 class FoodsController < ApplicationController
-    before_action :set_user, only: %i[index create destroy]
-    def index
-        @foods = @user.foods
-    end
+  before_action :set_user, only: %i[index create destroy]
+  def index
+    @foods = @user.foods
+  end
 
-    def new
-        @food = Food.new
-    end
+  def new
+    @food = Food.new
+  end
 
-    def create
-        @food = @user.foods.new(food_params)
-        if @food.save
-            redirect_to user_foods_path(@user)
-        else
-            render :new
-        end
+  def create
+    @food = @user.foods.new(food_params)
+    if @food.save
+      redirect_to user_foods_path(@user)
+    else
+      render :new
     end
+  end
 
-    def destroy
-        @food = @user.foods.find(params[:id])
-        @food.destroy
-        redirect_to user_foods_path(@user), notice: 'Food deleted successffuly'
-    end
+  def destroy
+    @food = @user.foods.find(params[:id])
+    @food.destroy
+    redirect_to user_foods_path(@user), notice: 'Food deleted successffuly'
+  end
 
-    private
+  private
 
-    def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
-    end
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 
-    def set_user
-        @user = User.find(params[:user_id])
-    end
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
