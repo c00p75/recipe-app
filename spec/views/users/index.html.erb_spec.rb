980c688f1440fd69377151d7747f00fb765c5fc8
c00p75 <georgecoopmsapenda@gmail.com>
2023-06-28 15:10:42 +0200

    Add intergration test for users

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,20 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'users/index', type: :view do
-  before(:each) do
-    assign(:users, [
-             User.create!(
-               name: 'Name'
-             ),
-             User.create!(
-               name: 'Name'
-             )
-           ])
+  before(:example) do
+    @user1 = User.create(name: "Gordon Ramsay", email: "chef0@masterchef.com", password: "123456", confirmed_at: Time.now)
+    @user2 = User.create(name: "Cook", email: "chef102@masterchef.com", password: "123456", confirmed_at: Time.now)
+    @users = [@user1, @user2] 
+    # Render the view
+    render
   end
 
-  it 'renders a list of users' do
-    render
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new('Name'.to_s), count: 2
+  it 'displays a list of Users' do
+    expect(rendered).to have_content 'Users'
+    expect(rendered).to have_content 'Gordon Ramsay'
+    expect(rendered).to have_content 'Cook'
+  end
+
+  it 'should have link to view foods' do
+    expect(rendered).to have_link('View Foods', href: user_foods_path(@user1))
+    expect(rendered).to have_link('View Foods', href: user_foods_path(@user2))
+  end
+
+  it 'should have link to view recipes' do
+    expect(rendered).to have_link('View Recipes', href: user_recipes_path(@user1))
+    expect(rendered).to have_link('View Recipes', href: user_recipes_path(@user2))
   end
 end
