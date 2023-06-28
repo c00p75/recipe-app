411c97a9d6ba54912c0918a484d4493bf4998f8b
yodit93 <yoditabebe9391@gmail.com>
2023-06-28 22:21:04 +0300

    Create recipe_food join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_28_110656) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_28_191441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_28_110656) do
   end
 
   create_table "recipe_foods", force: :cascade do |t|
-    t.integer "quantity"
+    t.string "quantity"
     t.bigint "recipe_id", null: false
     t.bigint "food_id", null: false
     t.datetime "created_at", null: false
