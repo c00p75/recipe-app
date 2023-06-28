4e1e8567a311088a9f9f80d291abe745d80f014f
yodit93 <yoditabebe9391@gmail.com>
2023-06-28 21:40:37 +0300

    Add associations for food and recipe models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :recipes
   validates :name, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-
+  has_many :foods
   # Validations
   validates :name, presence: true, length: { maximum: 250 }
   validates :description, presence: true
