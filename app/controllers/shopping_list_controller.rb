8579ba6f71435f29b4b0944b17222b3653fe8d01
yodit93 <yoditabebe9391@gmail.com>
2023-06-29 19:16:45 +0300

    Add hove effect

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -6,11 +6,11 @@ class ShoppingListController < ApplicationController
       recipe_food_quantities = recipe.recipe_foods.group(:food_id).sum(:quantity)
       recipe_food_quantities.each do |food_id, quantity|
         food = Food.find(food_id)
-        if food.quantity < quantity # if the available food amount is less than required amount
-          required_quantity = quantity - food.quantity # set the food quantity to the difference
-          shopping_list_item = { food: food, quantity: required_quantity } # create a hash with the food and the quantity
-          @shopping_list << shopping_list_item # add the food to the shopping list
-        end
+        next unless food.quantity < quantity # if the available food amount is less than required amount
+
+        required_quantity = quantity - food.quantity # set the food quantity to the difference
+        shopping_list_item = { food:, quantity: required_quantity } # create a hash with the food and the quantity
+        @shopping_list << shopping_list_item # add the food to the shopping list
       end
     end
     @total_price = 0
